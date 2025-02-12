0c4eff5b25f5e9041c426f89ec49129ca5aeb7f6
ROCKRCOIN <david@rockrcoin.org>
2025-02-12 12:59:58 +0000

    Add icon system type definitions

--- ROCKRLite-Core/src/components/icons/base/types.ts
+++ ROCKRLite-Core/src/components/icons/base/types.ts
@@ -1,1 +1,30 @@
+export interface IconProps {
+  size?: number;
+  color?: string;
+  className?: string;
+  onClick?: () => void;
+}
 
+export interface StepIconProps extends IconProps {
+  active?: boolean;
+  completed?: boolean;
+  editable?: boolean;
+  stepNumber?: number;
+}
+
+export type ExperienceStepType = 
+  | 'details'
+  | 'rks'
+  | 'calendar'
+  | 'location'
+  | 'hosts'
+  | 'participants'
+  | 'authentication'
+  | 'publish';
+
+export interface StepConfig {
+  id: ExperienceStepType;
+  label: string;
+  editable: boolean;
+  order: number;
+}
