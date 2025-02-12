561fc8b463abe4cf5c73d1b620204dd6740b5afa
ROCKRCOIN <david@rockrcoin.org>
2025-02-12 13:33:20 +0000

    Update index to export ExperienceStepIcon

--- ROCKRLite-Core/src/components/icons/index.ts
+++ ROCKRLite-Core/src/components/icons/index.ts
@@ -2,3 +2,4 @@ export * from './base/Icon';
 export * from './base/types';
 export * from './step-indicators/StepIndicator';
 export * from './step-indicators/config';
+export * from './experience/ExperienceStepIcon';  // Add this new line
